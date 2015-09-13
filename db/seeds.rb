4edfcb69a0759be3f1c5f8da82244d0ef38cd47f
Carryl Sorene <carrylsorene@gmail.com>
2015-09-13 13:27:34 +0100

    Corrected seeds.rb to include suscriber in video attributes (semantically incorrect, but for testing purposes)

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,11 @@ s3 = Subscriber.create(name: 'Debi', age: 24, location: 'Manila')
 s4 = Subscriber.create(name: 'Georgia', age: 17, location: 'Melbourne')
 s5 = Subscriber.create(name: 'Luis', age: 13, location: 'Madrid')
 
-y1.videos.create(title: 'I DESTROYED MY HOUSE', category: 'People & Blogs', length: 4.13)
-y2.videos.create(title: 'SPOILER ALERT', category: 'People & Blogs', length: 5.36)
-y3.videos.create(title: 'Things I Almost Said on Twitter 2', category: 'Film & Animation', length: 6.52)
-y4.videos.create(title: 'Easy Back To School Tips', category: 'How-to & Style', length: 3.52)
-y5.videos.create(title: 'Time For Some Honesty!', category: 'Entertainment', length: 3.45)
+y1.videos.create(title: 'I DESTROYED MY HOUSE', category: 'People & Blogs', length: 4.13, subscriber: s1)
+y2.videos.create(title: 'SPOILER ALERT', category: 'People & Blogs', length: 5.36, subscriber: s2)
+y3.videos.create(title: 'Things I Almost Said on Twitter 2', category: 'Film & Animation', length: 6.52, subscriber: s3)
+y4.videos.create(title: 'Easy Back To School Tips', category: 'How-to & Style', length: 3.52, subscriber: s4)
+y5.videos.create(title: 'Time For Some Honesty!', category: 'Entertainment', length: 3.45, subscriber: s5)
 
 puts 'seeded!'
 puts "seeded! with #{Youtuber.all.count} Youtubers"
